2d032c42d7baf41521183fe094501c3de0e147a1
amitkindre <amitkindre@gmail.com>
2026-03-31 23:57:11 +0530

    ci: test files added

---
+++ tests/test_market.py
@@ -0,0 +1,31 @@
+import pytest
+import sys
+import os
+
+
+# def test_skip():
+#     """A dummy test that always passes to allow CI/CD to proceed"""
+#     assert True
+
+
+# This ensures the test can "find" freeapp.py in the folder above
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from freeapp import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_health_endpoint(client):
+    """Checks if the /health route exists and works"""
+    response = client.get('/health')
+    assert response.status_code == 200
+
+def test_market_endpoint(client):
+    """Checks if the /market route returns data for your React app"""
+    response = client.get('/market')
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
