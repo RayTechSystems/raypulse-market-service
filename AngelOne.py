8a3f4b253b5afca0df0dc2f4c2a784a73a6bf927
amitkindre <amitkindre@gmail.com>
2026-04-03 02:05:07 +0530

    chore: updated print statement

--- AngelOne.py
+++ AngelOne.py
@@ -31,6 +31,7 @@ def login_to_angel():
     """Handles the full login flow including TOTP"""
     global smart_api
     try:
+        print("--- RAYPULSE STARTING ---", flush=True)
         print("Attempting Login...")
         smart_api = SmartConnect(api_key=API_KEY)
         
